9c904b0dad0add3ef76613e3146207eeef3bf13f
key1ime <mtg.slate@gmail.com>
2025-06-01 22:48:23 -0400

    Initial commit

---
+++ src/main/java/net/keylime36/modularweapons/ModularWeapons.java
@@ -0,0 +1,16 @@
+package net.keylime36.modularweapons;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModularWeapons implements ModInitializer {
+	public static final String MOD_ID = "modularweapons";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/keylime36/modularweapons/ModularWeaponsClient.java
@@ -0,0 +1,10 @@
+package net.keylime36.modularweapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModularWeaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
